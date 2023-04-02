aaf2983b05391d33e56f8ccc4c13d704a1f71f51
samantaavijit <samantaavijit.2000@gmail.com>
2023-04-02 10:27:57 +0530

    changes

--- src/component/Pages/Schedule/Schedule.jsx
+++ src/component/Pages/Schedule/Schedule.jsx
@@ -12,104 +12,146 @@ import OffersModal from "../Offers/OffersModal";
 import moment from "moment";
 import FlooringForBusiness from "../FlooringForBusiness/FlooringForBusiness";
 import { DATE_TIME_HELPER } from "../../../helper/Helper";
+import LoadingOverlay from "react-loading-overlay";
+import axios from "../../../config/axios";
+import message from "../../../config/message";
+import { useEffect } from "react";
 
 const Schedule = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const [loading, setLoading] = useState(false);
+  const [slots, setSlots] = useState([]);
 
   const [slotDate, setSlotDate] = useState(
     moment(new Date()).format(DATE_TIME_HELPER.DATE_FORMAT)
   );
 
+  useEffect(() => {
+    setLoading(true);
+
+    axios
+      .get(`/api/appointment/get-available-slot?date=${slotDate}`)
+      .then((res) => {
+        setLoading(false);
+
+        if (res.data.success) {
+          setSlots(res.data.slots);
+        } else {
+          message.error(res.data.message);
+        }
+      })
+      .catch((err) => {
+        setLoading(false);
+
+        message.error("Something Went Wrong!!!");
+      });
+  }, slotDate);
+
   return (
-    <div>
-      <OffersModal />
-      <div className="lg:flex md:flex justify-center">
-        <ScheduleCalender
-          selectedDate={selectedDate}
-          setSelectedDate={(e) => {
-            setSlotDate(moment(e).format(DATE_TIME_HELPER.DATE_FORMAT));
-            setSelectedDate(e);
+    <LoadingOverlay active={loading} spinner text="Loading ...">
+      <div>
+        <OffersModal />
+        <div className="lg:flex md:flex justify-center">
+          <ScheduleCalender
+            selectedDate={selectedDate}
+            setSelectedDate={(e) => {
+              setSlotDate(moment(e).format(DATE_TIME_HELPER.DATE_FORMAT));
+              setSelectedDate(e);
+            }}
+          />
+          <AvailableSchedule selectedDate={slotDate} slots={slots} />
+        </div>
+
+        {/* offer section */}
+        <div
+          className="hero lg:py-20"
+          style={{
+            background: `url(https://s7d4.scene7.com/is/image/MohawkResidential/alc_oldquarter_banner_5000x2083_300dpi?scl=2&hei=800&wid=2000&op_sharpen=1&op_brightness=-30)`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "100%,25%",
           }}
-        />
-        <AvailableSchedule selectedDate={slotDate} />
-      </div>
-
-      {/* offer section */}
-      <div
-        className="hero lg:py-20"
-        style={{
-          background: `url(https://s7d4.scene7.com/is/image/MohawkResidential/alc_oldquarter_banner_5000x2083_300dpi?scl=2&hei=800&wid=2000&op_sharpen=1&op_brightness=-30)`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "100%,25%",
-        }}
-      >
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <img src={offerImg} alt="" />
-          </div>
-        </div>
-      </div>
-
-      {/* discover section */}
-      <Discover />
-
-      {/* top item */}
-      <TopItems />
-
-      {/* branding section */}
-      <div className="text-center">
-        <div className="hero bg-base-300 lg:py-20">
-          <div className="hero-content flex-col lg:flex-row">
-            <img src={img1} className=" shadow-black shadow-2xl w-2/4" alt="" />
-            <div>
-              <h1 className="lg:text-5xl md:text-3xl text-2xl font-bold">
-                TOP QUALITY,NAME-BRAND FLOORING
-              </h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="hero  bg-base-300 lg:py-20">
+        >
           <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src={img2} className="shadow-black shadow-2xl w-2/4" alt="" />
-            <div>
-              <h1 className="lg:text-5xl md:text-3xl text-2xl font-bold">
-                LATEST STYLES AND COLORS
-              </h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
+            <div className="text-center lg:text-left">
+              <img src={offerImg} alt="" />
             </div>
           </div>
         </div>
-        <div className="hero bg-base-300 lg:py-20">
-          <div className="hero-content flex-col lg:flex-row">
-            <img src={img3} className=" shadow-black shadow-2xl w-2/4" alt="" />
-            <div>
-              <h1 className="lg:text-5xl md:text-3xl text-2xl font-bold">
-                DURABLE AND LONG LASTING
-              </h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
 
-      {/* financing section */}
-      <ShopHundreds />
-      {/* low price guarantee */}
-      <FlooringForBusiness />
-    </div>
+        {/* discover section */}
+        <Discover />
+
+        {/* top item */}
+        <TopItems />
+
+        {/* branding section */}
+        <div className="text-center">
+          <div className="hero bg-base-300 lg:py-20">
+            <div className="hero-content flex-col lg:flex-row">
+              <img
+                src={img1}
+                className=" shadow-black shadow-2xl w-2/4"
+                alt=""
+              />
+              <div>
+                <h1 className="lg:text-5xl md:text-3xl text-2xl font-bold">
+                  TOP QUALITY,NAME-BRAND FLOORING
+                </h1>
+                <p className="py-6">
+                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                  assumenda excepturi exercitationem quasi. In deleniti eaque
+                  aut repudiandae et a id nisi.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="hero  bg-base-300 lg:py-20">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+              <img
+                src={img2}
+                className="shadow-black shadow-2xl w-2/4"
+                alt=""
+              />
+              <div>
+                <h1 className="lg:text-5xl md:text-3xl text-2xl font-bold">
+                  LATEST STYLES AND COLORS
+                </h1>
+                <p className="py-6">
+                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                  assumenda excepturi exercitationem quasi. In deleniti eaque
+                  aut repudiandae et a id nisi.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="hero bg-base-300 lg:py-20">
+            <div className="hero-content flex-col lg:flex-row">
+              <img
+                src={img3}
+                className=" shadow-black shadow-2xl w-2/4"
+                alt=""
+              />
+              <div>
+                <h1 className="lg:text-5xl md:text-3xl text-2xl font-bold">
+                  DURABLE AND LONG LASTING
+                </h1>
+                <p className="py-6">
+                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                  assumenda excepturi exercitationem quasi. In deleniti eaque
+                  aut repudiandae et a id nisi.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* financing section */}
+        <ShopHundreds />
+        {/* low price guarantee */}
+        <FlooringForBusiness />
+      </div>
+    </LoadingOverlay>
   );
 };
 
