c5107aece8a6f08729e2c58b472115b30f2569e0
skikhtear <skikhtear@gmail.com>
2023-04-04 16:46:51 +0600

    lm tile

--- src/component/Pages/Offers/Schedule/AvailableSchedule.jsx
+++ src/component/Pages/Offers/Schedule/AvailableSchedule.jsx
@@ -4,26 +4,26 @@ import ScheduleOption from "./ScheduleOption";
 import moment from "moment";
 
 const AvailableSchedule = ({ selectedDate, slots, setLoading }) => {
-    const [appointment, setAppointment] = useState(false);
+  const [appointment, setAppointment] = useState(false);
 
-    return (
-        <section className="mt-5">
-            <p className="text-center text-primary font-bold">
-                Available Appointments on {moment(selectedDate).format("LL")}
-            </p>
-            <div className="grid gap-6 grid-cols-1 mt-6 ">
-                <ScheduleOption slots={slots} setAppointment={setAppointment} />
-            </div>
-            {appointment && (
-                <BookingModal
-                    selectedDate={selectedDate}
-                    slots={slots}
-                    setAppointment={setAppointment}
-                    setLoading={setLoading}
-                />
-            )}
-        </section>
-    );
+  return (
+    <section className="mt-5">
+      <p className="text-center text-primary font-bold">
+        Available Appointments on {moment(selectedDate).format("LL")}
+      </p>
+      <div className="grid gap-6 grid-cols-1 mt-6 ">
+        <ScheduleOption slots={slots} setAppointment={setAppointment} />
+      </div>
+      {appointment && (
+        <BookingModal
+          selectedDate={selectedDate}
+          slots={slots}
+          setAppointment={setAppointment}
+          setLoading={setLoading}
+        />
+      )}
+    </section>
+  );
 };
 
 export default AvailableSchedule;
