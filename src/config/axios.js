e54f1d6c6e2df4fa1eb5b40d0401f561c0c63d33
samantaavijit <samantaavijit.2000@gmail.com>
2023-04-16 19:40:20 +0530

    api url changes

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 const axios = Axios.create({
   // baseURL: "http://localhost:8003", //local
   // baseURL: "http://34.231.221.222/api/", //prod
-  baseURL: "http://lmtile.store/api/", //prod
+  baseURL: "https://lmtile.store/api/", //prod
 });
 
 export default axios;
