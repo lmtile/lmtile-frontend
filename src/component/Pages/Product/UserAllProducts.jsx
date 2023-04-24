59e5f483244049c7459e24c7d88c688829b38f97
samantaavijit <samantaavijit.2000@gmail.com>
2023-04-24 16:01:23 +0530

    add sub category in search filed

--- src/component/Pages/Product/UserAllProducts.jsx
+++ src/component/Pages/Product/UserAllProducts.jsx
@@ -11,6 +11,8 @@ import {
 import { Link, useLocation, useParams } from "react-router-dom";
 import ReactPaginate from "react-paginate";
 import "./UserAllProducts.css";
+import qs from "query-string";
+import _ from "lodash";
 
 export default function UserAllProducts() {
   const [loading, setLoading] = useState(false);
@@ -29,12 +31,17 @@ export default function UserAllProducts() {
   const [isCalling, setIscalling] = useState(1);
 
   const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
+  const queryParam = qs.parse(location.search);
 
-  const searchColor = searchParams.get("color");
+  const searchColor = queryParam["color"];
+  const searchSubCat = queryParam["type"];
 
   if (searchColor && color !== searchColor) {
     setColor(searchColor);
+  }
+
+  if (searchSubCat && select_sub_category !== searchSubCat) {
+    set_select_sub_category(searchSubCat);
   }
 
   useEffect(() => {
@@ -43,14 +50,14 @@ export default function UserAllProducts() {
       setIscalling(1 + isCalling);
     }
     getAllProduct();
-  }, [select_category, color, page]);
+  }, [select_category, color, page, select_sub_category]);
 
   const getAllProduct = () => {
     setLoading(true);
 
     axios
       .get(
-        `/api/product/get-all-products?per_page=${per_page}&page=${page}&category=${select_category}&color=${color}`
+        `/api/product/get-all-products?per_page=${per_page}&page=${page}&category=${select_category}&color=${color}&type=${select_sub_category}`
       )
       .then((res) => {
         setLoading(false);
@@ -90,7 +97,15 @@ export default function UserAllProducts() {
               sub_cat: cat.sub_cat,
             };
           });
+
+          let index = _.findIndex(allCatergory, ({ label }) => {
+            return label.toLowerCase() === select_category;
+          });
+
+          let { sub_cat } = allCatergory[index];
+
           setAllCatergory(allCatergory);
+          set_sub_cat(sub_cat);
         }
       })
       .catch((err) => {
@@ -98,6 +113,7 @@ export default function UserAllProducts() {
         message.error("Something went wrong!!!");
       });
   };
+
   return (
     <LoadingOverlay active={loading} spinner text="Loading ...">
       <OffersModal />
@@ -114,24 +130,26 @@ export default function UserAllProducts() {
 
             <div className="collapse-title font-bold">Categories</div>
             <div className="collapse-content">
-              {allCatergory.map((cat, key) => (
-                <div key={key}>
-                  <Link
-                    to={{
-                      pathname: `/products/${cat.label.toLowerCase()}`,
-                      search: `${color ? "color=" + color : ""} `,
-                    }}
-                    onClick={() => {
-                      set_sub_cat(cat.sub_cat);
-                      set_select_category(cat.label.toLowerCase());
-                      setPage(1);
-                    }}
-                    className="font-bold text-sm"
-                  >
-                    {cat.label}
-                  </Link>
-                </div>
-              ))}
+              {allCatergory.map((cat, key) => {
+                return (
+                  <div key={key}>
+                    <Link
+                      to={{
+                        pathname: `/products/${cat.label.toLowerCase()}`,
+                        search: qs.stringify(queryParam),
+                      }}
+                      onClick={() => {
+                        set_sub_cat(cat.sub_cat);
+                        set_select_category(cat.label.toLowerCase());
+                        setPage(1);
+                      }}
+                      className="font-bold text-sm"
+                    >
+                      {cat.label}
+                    </Link>
+                  </div>
+                );
+              })}
             </div>
           </div>
 
@@ -140,18 +158,29 @@ export default function UserAllProducts() {
               <input type="checkbox" />
               <div className="collapse-title font-bold">Type</div>
               <div className="collapse-content">
-                {sub_cat.map((cat, key) => (
-                  <div key={key}>
-                    <button
-                      onClick={() => {
-                        set_select_sub_category(cat.value);
-                      }}
-                      className=" font-bold text-sm top-0"
-                    >
-                      {cat.label}
-                    </button>
-                  </div>
-                ))}
+                {sub_cat.map((cat, key) => {
+                  const newQueryParam = {
+                    ...queryParam,
+                    type: cat.label,
+                  };
+                  return (
+                    <div key={key}>
+                      <Link
+                        to={{
+                          pathname: `/products/${select_category}`,
+                          search: qs.stringify(newQueryParam),
+                        }}
+                        onClick={() => {
+                          set_select_sub_category(cat.label);
+                          setPage(1);
+                        }}
+                        className="font-bold text-sm top-0"
+                      >
+                        {cat.label}
+                      </Link>
+                    </div>
+                  );
+                })}
               </div>
             </div>
           )}
@@ -160,23 +189,29 @@ export default function UserAllProducts() {
             <input type="checkbox" />
             <div className="collapse-title font-bold">Color</div>
             <div className="collapse-content">
-              {ProductColor.map((cat, key) => (
-                <div key={key}>
-                  <Link
-                    to={{
-                      pathname: `/products/${select_category}`,
-                      search: `color=${cat.value}`,
-                    }}
-                    className="font-bold text-sm"
-                    onClick={() => {
-                      setColor(cat.value);
-                      setPage(1);
-                    }}
-                  >
-                    {cat.label}
-                  </Link>
-                </div>
-              ))}
+              {ProductColor.map((cat, key) => {
+                const newQueryParam = {
+                  ...queryParam,
+                  color: cat.value,
+                };
+                return (
+                  <div key={key}>
+                    <Link
+                      to={{
+                        pathname: `/products/${select_category}`,
+                        search: qs.stringify(newQueryParam),
+                      }}
+                      className="font-bold text-sm"
+                      onClick={() => {
+                        setColor(cat.value);
+                        setPage(1);
+                      }}
+                    >
+                      {cat.label}
+                    </Link>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
@@ -230,174 +265,3 @@ export default function UserAllProducts() {
     </LoadingOverlay>
   );
 }
-
-// export default class UserAllProducts extends Component {
-//   constructor(props) {
-//     console.log(props);
-//     super(props);
-//     this.state = {
-//       isLoading: false,
-
-//       allCatergory: [],
-//       sub_cat: [],
-//       products: [],
-
-//       select_category: "",
-//       select_sub_category: "",
-
-//       per_page: 10,
-//       page: 1,
-//     };
-//   }
-
-//   componentDidMount = () => {
-//     this.getAllProduct();
-//     this.getAllcategory();
-//   };
-
-//   getAllProduct = () => {
-//     this.setState({ isLoading: true });
-
-//     let { per_page, page } = this.state;
-
-//     axios
-//       .get(`/api/product/get-all-products?per_page=${per_page}&page=${page}`)
-//       .then((res) => {
-//         this.setState({ isLoading: false });
-//         if (res.data.success) {
-//           let { products } = res.data;
-//           this.setState({ products });
-//         } else {
-//           message.error(res.data.message);
-//         }
-//       })
-//       .catch((err) => {
-//         console.error(err);
-//         this.setState({ isLoading: false });
-//         message.error("Something went wrong!!!");
-//       });
-//   };
-
-//   getAllcategory = () => {
-//     this.setState({ isLoading: true });
-
-//     axios
-//       .get("/api/category/get-all-category")
-//       .then((res) => {
-//         this.setState({ isLoading: false });
-//         if (res.data.success) {
-//           let { category } = res.data;
-//           let allCatergory = category.map((cat) => {
-//             return {
-//               label: cat.category,
-//               value: cat._id,
-//               sub_cat: cat.sub_cat,
-//             };
-//           });
-//           this.setState({ allCatergory });
-//         }
-//       })
-//       .catch((err) => {
-//         this.setState({ isLoading: false });
-//         console.error(err);
-//         message.error("Something went wrong!!!");
-//       });
-//   };
-//   render() {
-//     let { products, allCatergory, sub_cat } = this.state;
-//     return (
-//       <LoadingOverlay active={this.state.isLoading} spinner text="Loading ...">
-//         <OffersModal />
-
-//         <div className="lg:flex  bg-base-100">
-//           <div className="px-10 gap-x-5 mt-20">
-//             <input
-//               type="text"
-//               placeholder="Search"
-//               className="input input-bordered border-black shadow-2xl my-5"
-//             />
-
-//             <div className="collapse collapse-plus bg-base-100 shadow-black shadow-2xl mb-5">
-//               <input type="checkbox" />
-
-//               <div className="collapse-title font-bold">Categories</div>
-//               <div className="collapse-content">
-//                 {allCatergory.map((cat, key) => (
-//                   <div key={key}>
-//                     <Link
-//                       to={{
-//                         pathname: `/products/${cat.label.toLowerCase()}`,
-//                       }}
-//                       onClick={() => {
-//                         console.log(cat.label.toLowerCase());
-//                         this.setState({
-//                           select_category: cat.value,
-//                           sub_cat: cat.sub_cat,
-//                         });
-//                       }}
-//                       className="font-bold text-sm"
-//                     >
-//                       {cat.label}
-//                     </Link>
-//                   </div>
-//                 ))}
-//               </div>
-//             </div>
-
-//             {sub_cat.length > 0 && (
-//               <div className="collapse collapse-plus bg-base-100 shadow-black shadow-2xl mb-5">
-//                 <input type="checkbox" />
-//                 <div className="collapse-title font-bold">Type</div>
-//                 <div className="collapse-content">
-//                   {sub_cat.map((cat, key) => (
-//                     <div key={key}>
-//                       <button
-//                         onClick={() => {
-//                           this.setState({ select_sub_category: cat.value });
-//                         }}
-//                         className=" font-bold text-sm top-0"
-//                       >
-//                         {cat.label}
-//                       </button>
-//                     </div>
-//                   ))}
-//                 </div>
-//               </div>
-//             )}
-
-//             <div className="collapse collapse-plus bg-base-100 shadow-black shadow-2xl mb-5">
-//               <input type="checkbox" />
-//               <div className="collapse-title font-bold">Color</div>
-//               <div className="collapse-content">
-//                 {ProductColor.map((cat, key) => (
-//                   <div key={key}>
-//                     <button className=" font-bold text-sm">{cat.label}</button>
-//                   </div>
-//                 ))}
-//               </div>
-//             </div>
-//           </div>
-
-//           <div className="grid gap-x-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-20 mb-10 mt-20">
-//             {products.map((product, key) => {
-//               return (
-//                 <Link to={`/product-details/${product._id}`}>
-//                   <div className="hover:shadow-xl shadow-2xl shadow-black  p-5 bg-base-100">
-//                     <div>
-//                       <img
-//                         src={`${BUCKET_DOMAIN}${product.images[0]} `}
-//                         alt={product.name}
-//                       />
-//                       <p className="text-xl font-bold">{product.name}</p>
-//                       <p>{product.color}</p>
-//                     </div>
-//                   </div>
-//                 </Link>
-//               );
-//             })}
-//           </div>
-//         </div>
-//       </LoadingOverlay>
-//     );
-//   }
-// }
