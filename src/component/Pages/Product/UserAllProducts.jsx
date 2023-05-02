a6ebbce140ff5a64bf69ee321d7e0caeede27f8f
samantaavijit <samantaavijit.2000@gmail.com>
2023-05-02 20:07:48 +0530

    clear fil.ter changes

--- src/component/Pages/Product/UserAllProducts.jsx
+++ src/component/Pages/Product/UserAllProducts.jsx
@@ -138,6 +138,11 @@ export default function UserAllProducts() {
         search: "",
       });
       set_search("");
+      setColor("");
+      set_select_sub_category("");
+
+      const pathWithoutQuery = window.location.pathname;
+      window.history.replaceState({}, "", pathWithoutQuery);
     } else {
       setIsApplyFilter(true);
       set_search(filterData.search);
@@ -213,6 +218,7 @@ export default function UserAllProducts() {
                         onClick={() => {
                           set_select_sub_category(cat.label);
                           setPage(1);
+                          setIsApplyFilter(true);
                         }}
                         className="font-bold text-sm top-0 active:text-red-800"
                       >
@@ -245,6 +251,7 @@ export default function UserAllProducts() {
                       onClick={() => {
                         setColor(cat.value);
                         setPage(1);
+                        setIsApplyFilter(true);
                       }}
                     >
                       {cat.label}
