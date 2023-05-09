b776e56f1f08bc2cbb2b41627ad2c1e0e5a6a72d
skikhtear <skikhtear@gmail.com>
2023-05-09 20:58:21 +0600

    roomvo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
-import RoomvoAssistant from "./RoomVo/RoomvoAssistant";
 import router from "./Routes/Route/Route";
 
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="mx-auto">
       <RouterProvider router={router}></RouterProvider>
-      <div id="roomvoAssistant"><RoomvoAssistant /></div>
     </div>
   );
 }
